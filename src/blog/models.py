693afca1cec4306e1fe1b42c2efd063b788d05dc
henryfrstr <63148122+henryfrstr@users.noreply.github.com>
2020-12-29 21:49:19 +0300

    added __str__ methods

--- src/blog/models.py
+++ src/blog/models.py
@@ -5,6 +5,12 @@ from django.contrib.auth.models import User
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
+
 
 class Post(models.Model):
     OPTIONS = (
@@ -20,3 +26,6 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     status = models.CharField(max_length=10, choices=OPTIONS, default='d')
     slug = models.SlugField(blank=True, unique=True)
+
+    def __str__(self):
+        return self.title
