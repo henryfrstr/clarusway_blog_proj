e8cac29978581f3bdef6ec10bd347c5b15b62cc4
henryfrstr <63148122+henryfrstr@users.noreply.github.com>
2021-01-09 18:30:21 +0300

    added validation

--- src/users/views.py
+++ src/users/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .forms import RegistrationForm
 
-# Create your views here.
+
+def register(request):
+    form = RegistrationForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        # return redirect("login")
+
+    context = {
+        "form": form,
+    }
+
+    return render(request, "users/register.html", context)
