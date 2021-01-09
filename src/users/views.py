09e2c933df9ce55f7cb0d53ca79ada3093ee5282
henryfrstr <63148122+henryfrstr@users.noreply.github.com>
2021-01-09 18:48:41 +0300

    added profile page

--- src/users/views.py
+++ src/users/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from .forms import RegistrationForm
+from django.shortcuts import redirect, render
+from .forms import RegistrationForm, UserUpdateForm, ProfileUpdateForm
 
 
 def register(request):
@@ -14,3 +14,22 @@ def register(request):
     }
 
     return render(request, "users/register.html", context)
+
+
+def profile(request):
+    # obj = User.objects.get(id=id)
+    u_form = UserUpdateForm(request.POST or None, instance=request.user)
+    p_form = ProfileUpdateForm(
+        request.POST or None, request.FILES or None, instance=request.user.profile)
+
+    if u_form.is_valid() and p_form.is_valid():
+        u_form.save()
+        p_form.save()
+        return redirect(request.path)
+
+    context = {
+        "u_form": u_form,
+        "p_form": p_form
+    }
+
+    return render(request, "users/profile.html", context)
